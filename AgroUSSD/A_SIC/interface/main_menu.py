6e0d85b8e955f0e3b22c36b6f1e1f3d96fc5dbde
olusola <somorinolusola1@gmail.com>
2025-09-08 06:38:02 +0100

    updated code

--- AgroUSSD/A_SIC/interface/main_menu.py
+++ AgroUSSD/A_SIC/interface/main_menu.py
@@ -3,48 +3,103 @@ from pathlib import Path
 
 
 def display():
-    print("\nWelcome to AgroLife!\nWe connect farmers and buyers directly, skip middlemen, and get fair price for all.\n\n1. Register\n2. Login\n3. Exit")
+    print("\nWelcome to AgroLife!\nA place where farmers and buyers connect directly, skip middlemen, and get fair price for all.\n\n1. Register\n2. Login\n0. Exit")
 
 def handle_input(choice):
     if choice == 1:
-        user_registration = int(input("\nWelcome!\nWho are you?\n1. Farmer\n2. Buyer\n0. Back to main menu\nChoice: "))
+        user_registration = int(input("\nWelcome!\nDo you want to register as a farmer or buyer?\n1. Farmer\n2. Buyer\n00. Back to main menu\nChoice: "))
         if user_registration == 1:
             return farmer_registration()
             
         elif user_registration == 2:
             return buyer_registration()
             
-        elif user_registration == 0:
+        elif user_registration == 00:
             return reset_to_main()
+        
+    # elif choice == 2:
+    #     user_login = int(input("\nWho do you want to login as?\n1. Farmer\n2. Buyer\n00. Back to main menu\nchoice: "))
+    #     if user_login == 1:
+    #         # return farmer_login()
 
 
 def reset_to_main():
     return display()
 
 
+# Saving and loading user details in a jason file
+# Create a file if it doesn't exist
+workspace = Path("Farmer_Details")
+workspace.mkdir(exist_ok=True)
+path = workspace / "Farmer.json"
+
+# Save users to file
+def save_farmer_registration(path, farmer_details_dict):
+    with open(path, "a", newline="", encoding="utf-8") as f:
+        json.dump(farmer_details_dict, f, indent=4)
+    print("Information saved to data base.")
+
+# Load users form file if it exists
+def load_farmer_registration(path):
+    if path.exists():
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                loaded_data = json.load(f)
+                return loaded_data
+            
+        except json.JSONDecodeError:
+            return {}
+    return {}
+
+
+# user = load_farmer_registration(path)
+
+# def farmer_login():
+#     phnon_no = int(input("\nEnter your phone number\nPhone number: "))
+#     if phnon_no in save_farmer_registration(farmer_details_dict=)
+#     pin = int(input("\nEnter your 4-digit pin below\nPin: "))
+
+
+
+# Farmer registration
 def farmer_registration():
-    farmer_details_dict = {}
+    # farmer_details_dict = {}
+
     try:
         print("\n\tYou are about to register as a Farmer.")
-        farmer_name = input("Enter your name: ").title()
-        farmer_details_dict['Name'] = farmer_name
-
         farmer_phone_no = input("Enter phone number: ")
+        # user = load_farmer_registration(path)
+        # if farmer_phone_no in user:
+        #     print("This phone number is already registered. Try logging in.")
+        #     # return
+        
         if len(farmer_phone_no) != 11:
             print("Phone number must be 11 digits!")
-        else:
-            farmer_details_dict['Phone number'] = farmer_phone_no
+        # else:
+        #     farmer_details_dict['Phone number'] = farmer_phone_no
+
+        farmer_name = input("Enter your name: ").title()
+
+        # farmer_details_dict['Name'] = farmer_name
 
         farmer_loaction = input("Enter your location: ").title()
-        farmer_details_dict['Location'] = farmer_loaction
+        # farmer_details_dict['Location'] = farmer_loaction
 
-        farmer_pin = input("Create a 4 digit pin: ")
+        farmer_pin = input("Create a 4-digit pin: ")
         if len(farmer_pin) != 4:
             print("Pin must be 4 digits!")
-        else:
-            farmer_details_dict['Pin'] = farmer_pin
+        # else:
+        #     farmer_details_dict['Pin'] = farmer_pin
+        user = {
+            farmer_phone_no:{
+            "Name": farmer_name,
+            "Location": farmer_loaction,
+            "Pin": farmer_pin
+            }
+        }
 
-        save_farmer_registration(path, farmer_details_dict)
+        save_farmer_registration(path, user)
+        print(f"Registration successful! Welcone, {farmer_name}")
     except Exception as e:
         print("Error as", e)
     
@@ -76,17 +131,3 @@ def buyer_registration():
         print("Error as", e)
 
 
-workspace = Path("Farmer_Details")
-workspace.mkdir(exist_ok=True)
-path = workspace / "Farmer.json"
-
-def save_farmer_registration(path, farmer_details_dict):
-    farmer_details_dict ={}
-    with open(path, "w", newline="", encoding="utf-8") as f:
-        json.dump(farmer_details_dict, f, indent=2)
-    print("Information saved to data base.")
-
-def load_farmer_registration(path):
-    with open(path, "r", encoding="utf-8") as f:
-        loaded_data = json.load(f)
-        print(loaded_data)
